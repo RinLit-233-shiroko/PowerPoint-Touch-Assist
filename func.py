023010c1f3a96d804ee1e69a9d567ae62b91739a
MojaveHao <HTTcode2020@126.com>
2024-11-03 05:11:18 +0800

    feat: 更高DPI(250%和300%)
    feat: 正则表达式匹配窗口(适用于WPS等)
    
    feat: requirements.txt

--- func.py
+++ func.py
@@ -1,35 +1,45 @@
+import re
+
 import pyautogui
 import pygetwindow as gw
 from pynput import mouse
 from win32 import win32api
+
 import conf_file as conf
 
-#get分辨率
+# get分辨率
 screenX = win32api.GetSystemMetrics(0)
 screenY = win32api.GetSystemMetrics(1)
 
-#缩放比率
+# 缩放比率
 scale_rate = conf.dpi_dict[conf.read_conf('General', 'DPI')]
 
 ppt_window_title = conf.read_conf('General', 'PPT_Title')
 
-tsk_edge = screenY - 95*scale_rate
-ppt_menuWL = 95*scale_rate
-ppt_menuWR = screenX - 95*scale_rate
+tsk_edge = screenY - 95 * scale_rate
+ppt_menuWL = 95 * scale_rate
+ppt_menuWR = screenX - 95 * scale_rate
 
 is_pressed = False
 pressed_menuButton = False
-pos_mouse = [0,0]
+pos_mouse = [0, 0]
 
 count = 0
 
-#检测是否有PowerPoint放映
+
+# 检测是否有PowerPoint放映
 def is_powerpoint_showing():
     try:
-        windows = gw.getWindowsWithTitle(ppt_window_title)
-        return len(windows) > 0
+        if bool(conf.read_conf('General', 'use_regex')):
+            windows = gw.getWindowsWithTitle(ppt_window_title)
+            return len(windows) > 0
+        else:
+            windows = gw.getAllTitles()
+            for window in windows:
+                if re.search(conf.read_conf('General', 'DPI'), window):
+                    return True
     except Exception:
-        print(f'这是\n{Exception}\n喵')#QwQ
+        print(f'这是\n{Exception}\n喵')  # QwQ
     return False
 
 
@@ -48,13 +58,13 @@ def is_not_click_taskbar(mouse_y):
 
 
 def is_not_click_ppt_menubar(mouse_x):
-    if mouse_x >= ppt_menuWL and mouse_x <= ppt_menuWR:
+    if ppt_menuWL <= mouse_x <= ppt_menuWR:
         return True
     else:
         return False
 
 
-#点击事件
+# 点击事件
 def on_click(x, y, button, pressed):
     global is_pressed
     global pos_mouse
@@ -72,21 +82,22 @@ def on_click(x, y, button, pressed):
             else:
                 is_pressed = True
             # ##如果点击ppt工具栏，禁用下一次左键点击以操作弹出的菜单###
-        else:# 当放开时判定是否按下且符合条件
+        else:  # 当放开时判定是否按下且符合条件
             if is_pressed and pressed_menuButton is False and is_finger_not_slide(x, y) and is_powerpoint_showing():
                 pyautogui.press('space')
             elif count != 0:
                 count -= 1
             if count == 0:
-                pressed_menuButton = False 
+                pressed_menuButton = False
             is_pressed = False
-            
 
-#监听
+
+# 监听
 def main():
     print('程序运行中……\nRinLit_233OuO @bilibili')
     with mouse.Listener(on_click=on_click) as listener:
         listener.join()
 
+
 if __name__ == '__main__':
-    main()  
+    main()
--- shortcut.py
+++ shortcut.py
@@ -1,29 +1,36 @@
 import os
+
 from win32com.client import Dispatch
 
 program_name = 'PowerPoint 触屏辅助'
+
+
+def create_shortcut(desktop_folder, file_path, icon_path):
+    shortcut_path = os.path.join(desktop_folder, f'{program_name}.lnk')
+
+    # 创建快捷方式
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(shortcut_path)
+    shortcut.TargetPath = file_path
+    shortcut.WorkingDirectory = os.path.dirname(file_path)
+    shortcut.IconLocation = icon_path  # 设置图标路径
+    shortcut.save()
+
 
 def add_to_startup(file_path="", icon_path=""):
     if file_path == "":
         file_path = os.path.realpath(__file__)
     else:
-        file_path = os.path.abspath(file_path)#将相对路径转换为绝对路径
-    
+        file_path = os.path.abspath(file_path)  # 将相对路径转换为绝对路径
+
     if icon_path == "":
-        icon_path = file_path#如果未指定图标路径，则使用程序路径
+        icon_path = file_path  # 如果未指定图标路径，则使用程序路径
 
     # 获取启动文件夹路径
     startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-    # 快捷方式文件名
-    shortcut_path = os.path.join(startup_folder, f'{program_name}.lnk')
-    
     # 创建快捷方式
-    shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortCut(shortcut_path)
-    shortcut.Targetpath = file_path
-    shortcut.WorkingDirectory = os.path.dirname(file_path)
-    shortcut.IconLocation = icon_path#设置图标路径
-    shortcut.save()
+    create_shortcut(startup_folder, file_path, icon_path)
+
 
 def remove_from_startup():
     startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
@@ -31,49 +38,42 @@ def remove_from_startup():
     if os.path.exists(shortcut_path):
         os.remove(shortcut_path)
 
+
 def add_to_desktop(file_path="", icon_path=""):
     if file_path == "":
         file_path = os.path.realpath(__file__)
     else:
-        file_path = os.path.abspath(file_path)#将相对路径转换为绝对路径
-    
+        file_path = os.path.abspath(file_path)  # 将相对路径转换为绝对路径
+
     if icon_path == "":
-        icon_path = file_path#如果未指定图标路径，则使用程序路径
+        icon_path = file_path  # 如果未指定图标路径，则使用程序路径
     # 获取文件夹路径
     desktop_folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    # 快捷方式文件名
-    shortcut_path = os.path.join(desktop_folder, f'{program_name}.lnk')
-    
     # 创建快捷方式
-    shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortCut(shortcut_path)
-    shortcut.Targetpath = file_path
-    shortcut.WorkingDirectory = os.path.dirname(file_path)
-    shortcut.IconLocation = icon_path#设置图标路径
-    shortcut.save()
+    create_shortcut(desktop_folder, file_path, icon_path)
 
-def add_to_startmenu(file_path="", icon_path="", name='PowerPoint 触屏辅助', args = ''):
+
+def add_to_menu(file_path="", icon_path="", name='PowerPoint 触屏辅助', args=''):
     if file_path == "":
         file_path = os.path.realpath(__file__)
     else:
-        file_path = os.path.abspath(file_path)#将相对路径转换为绝对路径
-    
+        file_path = os.path.abspath(file_path)  # 将相对路径转换为绝对路径
+
     if icon_path == "":
-        icon_path = file_path#如果未指定图标路径，则使用程序路径
+        icon_path = file_path  # 如果未指定图标路径，则使用程序路径
     # 获取文件夹路径
     menu_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs')
     # 快捷方式文件名
     shortcut_path = os.path.join(menu_folder, f'{name}.lnk')
-    
+
     # 创建快捷方式
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(shortcut_path)
-    shortcut.Targetpath = file_path
+    shortcut.TargetPath = file_path
     shortcut.Arguments = args
     shortcut.WorkingDirectory = os.path.dirname(file_path)
-    shortcut.IconLocation = icon_path#设置图标路径
+    shortcut.IconLocation = icon_path  # 设置图标路径
     shortcut.save()
-
 
 
 if __name__ == '__main__':
